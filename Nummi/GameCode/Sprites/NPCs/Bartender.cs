b7fee1fbaee879f9bff67b8b8b8c0faebdda651d
dodomak3n <ec2244297@edinburghcollege.ac.uk>
2026-05-21 22:10:08 +0100

    Created bartender

---
+++ Nummi/GameCode/Sprites/NPCs/Bartender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nummi.GameCode.Sprites.NPCs
+{
+    public class Bartender : SpriteNPC
+    {
+        public Bartender(Game1 gameRoot, Texture2D texture, Vector2 position) : 
+            base(gameRoot, texture, position, false, 0f, 0f, 0f, null)
+        {
+
+        }
+    }
+}
